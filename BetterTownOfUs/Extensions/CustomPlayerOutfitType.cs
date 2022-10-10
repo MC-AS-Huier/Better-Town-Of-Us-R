f029ab514c82463ceead92f19ac97d85b2452293
operrochek <103575184+operrochek@users.noreply.github.com>
2022-10-10 15:18:36 +0700

    Add files via upload

---
+++ BetterTownOfUs/Extensions/CustomPlayerOutfitType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterTownOfUs.Extensions
+{
+    public enum CustomPlayerOutfitType 
+    {
+        Default,
+        Shapeshifted,
+        Morph,
+        Camouflage,
+        Swooper,
+        PlayerNameOnly
+
+    }
+}
